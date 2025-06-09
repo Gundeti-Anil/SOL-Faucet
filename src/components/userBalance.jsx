dce46df86e8ee024d67cfff8f6a2edc6adb53106
Gundeti-Anil <anil.gundeti2000@gmail.com>
2025-06-10 00:54:13 +0530

    (chore):added disconnet set to 0 balace

--- src/components/userBalance.jsx
+++ src/components/userBalance.jsx
@@ -1,24 +1,31 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useEffect, useState } from "react";
 
 export function ShowSolBalance() {
     const { connection } = useConnection();
+    const [balance, setBalance] = useState(0);
     const wallet = useWallet();
-
+    
+    useEffect(() => {
+        getBalance();
+    }, [wallet.publicKey]);
+    
     async function getBalance() { 
         if (wallet.publicKey) {
 
             const balance = await connection.getBalance(wallet.publicKey);
-            document.getElementById("balance").innerHTML = balance / LAMPORTS_PER_SOL;
+            setBalance(balance / LAMPORTS_PER_SOL);
+        }else{
+            setBalance(0);
         }
     }
     
-    getBalance();
     return (
         <div className="bg-gradient-to-r from-green-600 to-emerald-600 rounded-xl p-6 shadow-lg">
             <div className="text-center">
                 <p className="text-green-100 text-sm font-medium mb-2">SOL Balance</p>
-                <p id="balance" className="text-white text-3xl font-bold">0</p>
+                <p className="text-white text-3xl font-bold">{balance}</p>
                 {/* <p className="text-green-100 text-sm mt-1">≈ $52.34 USD</p> */}
             </div>
         </div>
